907db4b6662322f7c094db923413506a2ccfe44b
Monocerum <monoceruscaelum@gmail.com>
2023-09-19 16:15:05 +0800

    Add option to change color

--- script.js
+++ script.js
@@ -12,6 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
         element.addEventListener('click', changeColorOnHover);
         element.addEventListener('mouseenter', changeColorOnHover);
     });
+
+    const options = document.querySelector('select');
+
+    options.addEventListener('change', updateColor);
 });
 
 function promptSize() {
@@ -68,5 +72,55 @@ function createGrid(gridSize) {
 
 function changeColorOnHover(e) {
     e.target.classList.add('hover');
+
+    let color = checkColor();
+
+    console.log(color);
+
+    if (e.target.classList.contains('hover')) {
+        e.target.style.backgroundColor = color;
+    }
 }
+
+function checkColor() {
+    const options = document.querySelector('select');
+
+    let color = options.value;
+    let newColor = '#36454F';
+
+    switch (color) {
+        case 'black':
+            newColor = 'black';
+            break;
+        case 'navy-blue':
+            newColor = '#000080';
+            break;
+        case 'light-blue':
+            newColor = '#B1F2FF';
+            break;
+        case 'pink':
+            newColor = '#FFC0CB';
+            break;
+        case 'charcoal':
+            newColor = '#36454F';
+            break;
+        default:
+            newColor = '#36454F';
+            break;
+    }
+
+    return newColor;
+}
+
+function updateColor() {
+    
+    const gridElements = document.querySelectorAll('.grid-element.hover');
+
+    let color = checkColor();
+
+    gridElements.forEach((element) => {
+        element.style.backgroundColor = color;
+    })
+}
+
     
